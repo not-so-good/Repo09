5de00b5c1516e87379ea70e1dae3aa5cd194c95d
Sanjin Suljanovic <sanjin.suljanovic@gmail.com>
2022-04-06 00:18:26 +0200

    FOR loops

---
+++ ex18/src/index.js
@@ -0,0 +1,33 @@
+//Only change code below this line
+function myForLoop1() {
+    var evenNumbers = "";
+    for (var i = 0; i <= 8; i++) {
+        var x;
+        x = i % 2;
+        if (x == 0){
+            evenNumbers += i;
+        if(i != 8)
+            evenNumbers += ", "
+        }
+    }
+    return evenNumbers;
+}
+function myForLoop2() {
+    var evenInverseNumbers = "";
+    for (var i = 8; i >= 0; i--) {
+        var x;
+        x = i % 2;
+        if (x == 0){
+            evenInverseNumbers += i;
+        if(i != 0)
+            evenInverseNumbers += ", "
+        }
+    }
+    return evenInverseNumbers;
+    //Only change code above this line 
+}
+
+console.log(myForLoop1());
+console.log(myForLoop2());
+
+module.exports = { myForLoop1, myForLoop2 }; 
