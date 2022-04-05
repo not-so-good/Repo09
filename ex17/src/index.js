33f1d1e2f5403513fa6de8a579764afee2534968
Sanjin Suljanovic <sanjin.suljanovic@gmail.com>
2022-04-05 23:53:39 +0200

    While loops

---
+++ ex17/src/index.js
@@ -0,0 +1,15 @@
+  //Only change code below this line
+function inverseWhile() {
+    var fiveNumbers = "";
+    var i = 5;
+    while (i >= 0){
+        fiveNumbers += i;
+        if(i != 0)
+        fiveNumbers += ", "
+        i--;
+    }
+    return fiveNumbers;
+}
+//Only change code above this line 
+console.log(inverseWhile());
+module.exports = inverseWhile; 
